addc24bac2b4e69e22a2236740e1c0f65a2707ec
Nedeljko Mijajlovic <stoopro2@gmail.com>
2018-11-16 10:20:51 +0100

    Initial Commit

---
+++ HellooooWorld/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HellooooWorld
+//
+//  Created by Nedeljko Mijajlovic on 16.11.2018.
+//  Copyright © 2018 Nedeljko Mijajlovic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
