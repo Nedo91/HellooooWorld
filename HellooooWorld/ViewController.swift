8a95c5da39ee14ccce45213ea6cfe6507103bdfe
Nedeljko Mijajlovic <stoopro2@gmail.com>
2018-11-17 11:06:47 +0100

    add files

--- HellooooWorld/ViewController.swift
+++ HellooooWorld/ViewController.swift
@@ -10,11 +10,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var background: UIImageView!
+    
+    @IBOutlet weak var titleImage: UIImageView!
+    
+    @IBOutlet weak var welcomeBtn: UIButton!
+    
+    @IBOutlet weak var backBtn: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func welcomePressed(_ sender: Any) {
+        
+        background.isHidden = false
+        titleImage.isHidden = false
+        welcomeBtn.isHidden = true
+        backBtn.isHidden = false
+    }
+    
+    @IBAction func backPressed(_ sender: Any) {
+        background.isHidden = true
+        titleImage.isHidden = true
+        welcomeBtn.isHidden = false
+        backBtn.isHidden = true
+    }
 }
 
